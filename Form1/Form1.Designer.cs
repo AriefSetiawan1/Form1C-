7205229bbd849403fb1c230280749e2c906434b5
Arief <ariefstwn3@gmail.com>
2021-10-29 20:02:16 +0700

    first commit

---
+++ Form1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace Form1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TextUser = new System.Windows.Forms.MaskedTextBox();
+            this.TextPass = new System.Windows.Forms.MaskedTextBox();
+            this.Loginbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(489, 73);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login Page";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(121, 182);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(122, 265);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password :";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // TextUser
+            // 
+            this.TextUser.Location = new System.Drawing.Point(242, 179);
+            this.TextUser.Name = "TextUser";
+            this.TextUser.Size = new System.Drawing.Size(298, 27);
+            this.TextUser.TabIndex = 3;
+            // 
+            // TextPass
+            // 
+            this.TextPass.Location = new System.Drawing.Point(242, 258);
+            this.TextPass.Name = "TextPass";
+            this.TextPass.Size = new System.Drawing.Size(298, 27);
+            this.TextPass.TabIndex = 4;
+            // 
+            // Loginbtn
+            // 
+            this.Loginbtn.Location = new System.Drawing.Point(494, 355);
+            this.Loginbtn.Name = "Loginbtn";
+            this.Loginbtn.Size = new System.Drawing.Size(124, 43);
+            this.Loginbtn.TabIndex = 5;
+            this.Loginbtn.Text = "Login";
+            this.Loginbtn.UseVisualStyleBackColor = true;
+            this.Loginbtn.Click += new System.EventHandler(this.Loginbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 701);
+            this.Controls.Add(this.Loginbtn);
+            this.Controls.Add(this.TextPass);
+            this.Controls.Add(this.TextUser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox TextUser;
+        private System.Windows.Forms.MaskedTextBox TextPass;
+        private System.Windows.Forms.Button Loginbtn;
+    }
+}
+
---
+++ Form1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Loginbtn_Click(object sender, EventArgs e)
+        {
+            if (TextUser.Text == "" || TextPass.Text == "")
+            {
+                MessageBox.Show("Username dan Password tidak boleh kosong");
+            }
+            if (TextUser.Text != "admin" || TextPass.Text != "12345")
+            {
+                MessageBox.Show("Username atau Password salah");
+            }
+            else
+            {
+                this.Hide();
+                Sukses s = new Sukses();
+                s.Show();
+            }
+        }
+    }
+}
